990508cc00c623c1b782134db1beb849d66fd93e
Sonata <ostarablue@gmail.com>
2020-02-25 16:40:07 -0500

    finish flights

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -5,27 +5,39 @@ class FlightsController < ApplicationController
     end
 
     def show
-
+        @flight = Flight.find(params[:id])
+        
     end
 
     def new
-
+        @flight = Flight.new
     end
 
     def create
-
+        new_flight = Flight.create(flight_params(:origin, :destination, :dept_time, :airline))
+        redirect_to flights_path
     end
 
     def edit
-
+        @flight = Flight.find(params[:id])
     end
 
     def update
-
+        @flight = Flight.find(params[:id])
+        @flight.update(flight_params(:origin, :destination, :dept_time, :airline))
+        redirect_to flight_path(@flight)
     end
 
-    def delete
+    def destroy
+        @flight = Flight.find(params[:id])
+        @flight.destroy
+        redirect_to flights_path
+    end
 
+    private
+
+    def flight_params(*args)
+        params.require(:flight).permit(*args)
     end
 
 end
